ddfcdcc6d927e7ec45e73f3366c04fe85d084f65
Monica Delgado <mncawebdev@gmail.com>
2023-10-05 21:48:44 -0500

    starting point for portfolio

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+
+import React, { Fragment } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <Fragment>
+     <p>My portfolio!</p>
+    </Fragment>
+  )
+}
+
+export default App
